bc6a02889cb835bf68baee902e0d5f0a41288c09
SeekerRook <xrist.vardakis@gmail.com>
2021-11-30 18:39:34 +0200

    fixed better complexity shops.cpp

--- lab01/shops.cpp
+++ lab01/shops.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 unordered_multimap <int,pair<int,int>> Sets;
 int buildings[20000];
-
+set <long> res;
 long N,K;
+
 
 void preprocess (int N){
     for (int i = 0; i<N; i++){
@@ -21,23 +22,40 @@ void preprocess (int N){
 }
 
 int Search(int n){
-
+        
         // for (int i = 0; i<n; i++){
          //   cout << i << "\n";
         unordered_multimap<int,pair<int,int>> :: iterator itr;
         for( itr = Sets.begin(); itr!= Sets.end(); itr++ ){
 
-            if (itr->first == K) return itr->second.second - itr->second.first +1;
-            unordered_multimap<int,pair<int,int>> :: iterator f = Sets.find(K - itr->first) ;
-            if( f != Sets.end()){
-                if (f->second.first > itr->second.second ) {
-                    return itr->second.second - itr->second.first + f->second.second - f->second.first +2;
+            if (itr->first == K) {res.insert( itr->second.second - itr->second.first +1); return 1;}
+            // unordered_multimap<int,pair<int,int>> :: iterator f = Sets.find(K - itr->first) ;
+            // if( f != Sets.end()){
+            //     if (f->second.first > itr->second.second ) {
+            //         return itr->second.second - itr->second.first + f->second.second - f->second.first +2;
+            //     }
+            // }
+            auto range = Sets.equal_range(K-itr->first);
+            if (range.first != range.second){
+                // cout << "itr\n";
+                // cout << itr->second.first<<"/";
+                // cout << itr -> second.second<<"\n";
+            
+                for(auto i = range.first; i != range.second; i++){
+                        // cout << i->second.first<<"/";
+                        // cout << i -> second.second<<"\n";
+                    if (i->second.first > itr->second.second || itr->second.first > i->second.second){
+
+                        res.insert(itr->second.second - itr->second.first + i->second.second - i->second.first +2);
+                    }
                 }
-            }
+            // return 1;
         }
 
-    // }
+    }
+    //res.insert(-1);
     return -1;
+
 }
 int main(){
      cin >> N >> K ;
@@ -47,13 +65,14 @@ int main(){
     }
     preprocess(N);
 
-    int res;
-    for (int i = 1 ; i<= N; i++){
-        res = Search(i) ;
-        if (res != -1)break;
-    }
+    int r;
+    // for (int i = 1 ; i<= N; i++){
+        r = Search(1) ;
+        // if (r != -1)break;
+    // }
 
-    
-    cout << res <<'\n';
+    if (*res.begin() == 0) cout << -1 <<'\n';
+    else
+    cout << *res.begin() <<'\n';
 
 }
